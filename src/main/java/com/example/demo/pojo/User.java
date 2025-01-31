0a9f493725e84c47192c8a9564f184f4e5eda7c4
quiteqiang <qiangwang2@outlook.com>
2025-02-01 00:06:30 +0800

    draft db mapping

---
+++ src/main/java/com/example/demo/pojo/User.java
@@ -0,0 +1,60 @@
+package com.example.demo.pojo;
+
+import jakarta.persistence.*;
+
+@Table(name="tb_user")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Integer userId;
+    @Column(name="user_name")
+    private String userName;
+    @Column(name="password")
+    private String password;
+    @Column(name="email")
+    private String email;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+
+    public void setUsername(String username) {
+        this.userName = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
